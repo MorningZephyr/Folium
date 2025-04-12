93cd84a96f4444d84ee612fe4b053db7ff7e67ca
Zephyr <zhentaop118@gmail.com>
2025-04-12 19:43:12 -0400

    successfully loaded pdf on website, although quality needs improvement, and so need text overlay

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,11 @@
+import PDFReader from "@/components/PDFReader";
+
 
 export default function Home() {
-  return (<div>Hello</div>);
+  return (
+  
+  
+    <PDFReader/>
+      
+  );
 }
--- src/components/PDFReader.js
+++ src/components/PDFReader.js
@@ -5,21 +5,90 @@
     
 'use client'
 
-import {useRef, useState} from 'react'
+import {useRef} from 'react'
+import { getDocument, GlobalWorkerOptions } from 'pdfjs-dist';
+GlobalWorkerOptions.workerSrc = 'https://unpkg.com/pdfjs-dist@5.1.91/build/pdf.worker.min.mjs';
 
 export default function PDFReader() {
-    
-    const canvasRef = useRef(null)
+
+    const canvasRef = useRef(null);
+
+    // Handles what happens when user uploads a PDF
+    const handleFileChange = (e) => {
+
+        const file = e.target.files[0];
+
+        // No file or invalid file type, warn user
+        if (!file || file.type !== 'application/pdf')  {
+            alert("No file or invalid file type");
+            return;
+        }
+
+        console.log('User uploaded:', file.name);
+
+        // Initialize Reader from JS
+        const reader = new FileReader();
+
+        // Tells reader what to do after the file reading from below is finished
+        reader.onload = async function () {
+
+            console.log('File loaded');
+            console.log(this.result);       // this.result is the ArrayBuffer of binaries of the pdf
+            
+            // Converting the raw binaries to Uint8Array for PDFjs
+            const typedArray = new Uint8Array(this.result);
+            console.log('First few bytes of the file:', typedArray.slice(0, 10));   // For tesing purpose, privacy concerns
+
+            // Loading and parsing the PDF, aka PDFLoadingTask object representing the in-process load
+            const loadingTask = getDocument({data: typedArray});
+
+            // When loading is finished, store the parsed PDF. It is now a PDFDocumentProxy object of the PDFjs
+            const loadedPDF = await loadingTask.promise;
+            console.log('PDF loaded');
+
+            // Getting the first page
+            const page = await loadedPDF.getPage(1);
+            // ???
+            const viewport = page.getViewport({scale: 1});
 
 
-    // For when user uploads a file
-    const handleFileChange = async (e) => {
-        const file = e.target.files[0]                              // .files is a built=in props, we just need the first file                           
+            // --- Canva config
 
-        if (!file || file.type !== 'application/pdf') return        // No file enter or invalid format
 
-        const reader = new FileReader()
+            const canvas = canvasRef.current;
+            const context = canvas.getContext('2d');
 
-        reader.onload = async function 
+            canvas.width = viewport.width
+            canvas.height = viewport.height;
+
+            const renderContext = {
+                canvasContext: context, viewport,
+            };
+
+            page.render(renderContext);
+
+        }
+
+        // Reader will read the file as pure raw binary
+        reader.readAsArrayBuffer(file);
+
+
+
+
+
+
     }
+
+    return (
+        <div>
+            <input 
+                type='file' 
+                accept='application/pdf' 
+                onChange={handleFileChange}
+            />
+
+            <canvas ref={canvasRef}></canvas>
+
+        </div>
+    )
 }
