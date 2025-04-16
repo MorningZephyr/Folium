f9388ee07a9e973d3f085a74b0d17c8fef0ed2a9
Zephyr <zhentaop118@gmail.com>
2025-04-16 18:19:23 -0400

    PDFjs successful able to parse the right number of pages of given PDF

--- src/components/PDFReader.js
+++ src/components/PDFReader.js
@@ -15,6 +15,7 @@ export default function PDFReader() {
 
   const [selectedFile, setSelectedFile] = useState(null);
   const [fileName, setFileName] = useState('');
+  const [numPages, setNumPages] = useState(0)
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [pdfDoc, setPdfDoc] = useState(null)
@@ -53,29 +54,54 @@ export default function PDFReader() {
       try {
         const reader = new FileReader();
 
-        // --- Defines what to do when reader finishes reading file ---
+        // --- Defines what to do when reader finishes reading file (Setup for PDfjs) ---
         reader.onload = async (e) => {
 
-          
+          try {
+            
+            // May happen when reading file is cancelled midway, or user upload 0 byte file
+            if (!e.target?.result) throw new Error("Failed to read file"); 
+
+            // Convert so pdfjs can interpret
+            const typedArray = new Uint8Array(e.target.result);
+
+            // PDFjs parsing the uint8array. Asynchronous, so immediately returns a 'loadingTask' object (similar to promise)
+            const loadingTask = pdfjsLib.getDocument({data: typedArray});
+
+            // Waits for the loading to finish. Result is a PDFDocumentProxy object, the parsed PDF
+            const loadedPdfDoc = await loadingTask.promise;
+
+            // Set the finished PDF to the state 
+            setPdfDoc(loadedPdfDoc);
+            setNumPages(loadedPdfDoc.numPages);
+            setIsLoading(false);
+
+          } catch (loadErr) {   // Catches error when loading and processing PDF into reader
+            console.error("Error occurred in reader:", loadErr);
+            setIsLoading(false);
+            setError(`Error! Reader unable to load PDF: ${loadErr.message}`);
+            setPdfDoc(null);
+          }
+
 
         }
 
         // --- Defines what to do when reader has error ---
-        reader.onerror = (e) => {
+        reader.onerror = (e) => {   // Handles Error such as locked PDF/permission issues
           
-          console.error("FileReader error:", e);
-          setError("Failed to read file");
           setIsLoading(false);
+          console.error("FileReader error:", e);
+          setError("Failed to read file. File may be locked.");
 
         }
 
         // --- Reader reads raw file bytes ---
         reader.readAsArrayBuffer(selectedFile);
       
-      } catch (err) {
+      } catch (err) {     // Errors that occur when setting up the FileReader
 
         console.error("Error setting up FileReader:", err);
-        setError(`Failed setup: ${err.message}`);
+        setError(`Failed to set up the FileReader: ${err.message}`);
         setIsLoading(false);
 
       }
@@ -83,7 +109,6 @@ export default function PDFReader() {
     }
 
     loadPDF();
-
 
   }, [selectedFile])  // Only rerenders when users add/change file
 
@@ -100,6 +125,8 @@ export default function PDFReader() {
         />
 
         {fileName && <p>Selected file: {fileName}</p>}
+        <p>User uploaded: {fileName}</p>
+        <p>Number of pages: {numPages}</p>
 
     </div>
     
